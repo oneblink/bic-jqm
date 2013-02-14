ef6801e50ba7699849b001dba2fe25f1c0e495a5
Ben Barclay <ben.barclay@blinkmobile.com.au>
2013-02-15 09:48:24 +1100

    Support for Collection B built JS bundle

--- source/main.js
+++ source/main.js
@@ -4,13 +4,13 @@ requirejs.config({
         text: ['/_BICv3_/assets/js/text', 'https://raw.github.com/requirejs/text/latest/text'],
         jquery: ['/_BICv3_/assets/js/jquery.min', 'https://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min'],
         jquerymobile: ['/_BICv3_/assets/js/jquery.mobile.min', 'http://code.jquery.com/mobile/1.2.0/jquery.mobile-1.2.0.min.js'],//'/_BICv3_/assets/js/jquery.mobile'
-        underscore: ['/_BICv3_/assets/js/underscore-min', 'http://underscorejs.org/underscore-min'],
-        backbone: ['/_BICv3_/assets/js/backbone', '../assets/js/backbone-min', 'http://backbonejs.org/backbone-min'],
-        mustache: ['/_BICv3_/assets/js/mustache', 'https://raw.github.com/janl/mustache.js/master/mustache'],
+        underscore: ['/_BICv3_/assets/js-built/CollectionB.min'],
+        backbone: ['/_BICv3_/assets/js-built/CollectionB.min'],
+        mustache: ['/_BICv3_/assets/js-built/CollectionB.min'],
         json2: ['/_BICv3_/assets/js/json2'],
-        lawnchair: ['/_BICv3_/assets/js/lawnchair'],
+        lawnchair: ['/_BICv3_/assets/js-built/CollectionB.min'],
         'lawnchair-indexed-db': ['/_BICv3_/assets/js/lawnchair-indexed-db'],
-        'lawnchair-webkit-sqlite': ['/_BICv3_/assets/js/lawnchair-webkit-sqlite'],
+        'lawnchair-webkit-sqlite': ['/_BICv3_/assets/js-built/CollectionB.min'],
         BForms: ['/_BICv3_/assets/js/BlinkForms-jQueryMobile']
     },
     shim: {
