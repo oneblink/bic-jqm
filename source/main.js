707cd8663269baf4ee8ef545e001fcad12095bf4
Ben Barclay <ben.barclay@blinkmobile.com.au>
2013-02-19 15:45:27 +1100

    Cleaning up dev library file paths

--- source/main.js
+++ source/main.js
@@ -1,16 +1,16 @@
 requirejs.config({
     baseUrl: '/_BICv3_/source',
     paths:{
-        text: ['/_BICv3_/assets/js/text', 'https://raw.github.com/requirejs/text/latest/text'],
-        jquery: ['/_BICv3_/assets/js/jquery.min', 'https://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min'],
-        jquerymobile: ['/_BICv3_/assets/js/jquery.mobile.min', 'http://code.jquery.com/mobile/1.2.0/jquery.mobile-1.2.0.min.js'],//'/_BICv3_/assets/js/jquery.mobile'
-        underscore: ['/_BICv3_/assets/js-built/CollectionB.min'],
-        backbone: ['/_BICv3_/assets/js-built/CollectionB.min'],
-        mustache: ['/_BICv3_/assets/js-built/CollectionB.min'],
+        text: ['/_BICv3_/assets/js/text'],
+        jquery: ['/_BICv3_/assets/js/jquery.min'],
+        jquerymobile: ['/_BICv3_/assets/js/jquery.mobile.min'],
+        underscore: ['/_BICv3_/assets/js/underscore-min'],
+        backbone: ['/_BICv3_/assets/js/backbone'],
+        mustache: ['/_BICv3_/assets/js/mustache'],
         json2: ['/_BICv3_/assets/js/json2'],
-        lawnchair: ['/_BICv3_/assets/js-built/CollectionB.min'],
+        lawnchair: ['/_BICv3_/assets/js/lawnchair'],
         'lawnchair-indexed-db': ['/_BICv3_/assets/js/lawnchair-indexed-db'],
-        'lawnchair-webkit-sqlite': ['/_BICv3_/assets/js-built/CollectionB.min'],
+        'lawnchair-webkit-sqlite': ['/_BICv3_/assets/js/lawnchair-webkit-sqlite'],
         BForms: ['/_BICv3_/assets/js/BlinkForms-jQueryMobile']
     },
     shim: {
