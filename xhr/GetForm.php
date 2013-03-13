efa8a68dec5bff3ea0c0605201d529a788172798
Ben Barclay <ben.barclay@blinkmobile.com.au>
2013-03-13 15:20:46 +1100

    Sample subform

--- xhr/GetForm.php
+++ xhr/GetForm.php
@@ -17,6 +17,9 @@ switch($_REQUEST['form']){
     case "Sample5":
         $content = '{"definition":{"default":{"name":"form1","label":"Form 1","_elements":[{"default":{"name":"id","type":"hidden"}},{"default":{"name":"url","label":"URL","type":"url","defaultValue":"https://blinkm.co/ron"}},{"default":{"name":"email","label":"Email","type":"email","defaultValue":"ron@blinkmobile.com.au","section":"account"}},{"default":{"name":"password","label":"Password","type":"password","defaultValue":"secret","section":"account"}},{"default":{"name":"streetAddress","label":"Street Address","type":"textarea","defaultValue":"Suite 2\r\n125 Donnison Street","section":"address"}},{"default":{"name":"city","label":"City","type":"text","defaultValue":"Gosford","section":"address","toolTip":"Enter City Name","required":true}},{"default":{"name":"telephone","label":"Telephone","type":"telephone","defaultValue":"+61 439 901 787"}},{"default":{"name":"number","label":"Number","type":"number","min":15,"max":400,"step":5,"defaultValue":35,"maxDecimalPlaces":2,"prefix":"$"}},{"default":{"name":"currency","label":"Currency","type":"number","minDecimals":2,"maxDecimals":2,"defaultValue":"876.54"}},{"default":{"name":"heading","type":"heading","level":1,"text":"Heading!"}},{"default":{"name":"message","type":"message","html":"This is <strong>static</strong> text."}}],"_sections":[{"default":{"name":"account","class":"myClass"}},{"default":{"name":"address","class":"myClass"}}]}}}';
         break;
+    case "form2":
+        $content = '{"definition":{"default":{"name":"form2","label":"Form 2","_elements":[{"default":{"name":"id","type":"hidden"}},{"default":{"name":"comment","label":"Comment","type":"textarea"}}]}}}';
+        break;
     default:
         $content = "";
         break;
