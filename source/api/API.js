4ca37c147a13c07db188bd7ae6249b6a643f9497
Ben Barclay <ben.barclay@blinkmobile.com.au>
2013-03-28 11:05:47 +1100

    Remove unneeded options

--- source/api/API.js
+++ source/api/API.js
@@ -3,7 +3,7 @@ define(
   function ($) {
     "use strict";
     var API = {
-      getInteraction: function (answerspace, interaction, args, options) {
+      getInteraction: function (answerspace, interaction, args) {
         var argstring = '';
         if (args) {
           $.each(args, function (key, value) {
@@ -13,15 +13,15 @@ define(
         return $.ajax('/_BICv3_/xhr/GetInteraction.php?asn=' + answerspace + '&iact=' + interaction + argstring);
       },
 
-      getAnswerSpace: function (answerspace, options) {
+      getAnswerSpace: function (answerspace) {
         return $.ajax('/_BICv3_/xhr/GetApp.php?asn=' + answerspace);
       },
 
-      getDataSuitcase: function (answerspace, datasuitcase, options) {
+      getDataSuitcase: function (answerspace, datasuitcase) {
         return $.ajax('/_BICv3_/xhr/GetDataSuitcase.php?asn=' + answerspace + '&ds=' + datasuitcase);
       },
 
-      getForm: function (answerspace, form, options) {
+      getForm: function (answerspace, form) {
         return $.ajax('/_BICv3_/xhr/GetForm.php?asn=' + answerspace + '&form=' + form);
       }
     };
