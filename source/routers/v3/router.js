58cfa142c481c2260a59cfeb9a4c3be440de1584
Ben Barclay <ben.barclay@blinkmobile.com.au>
2013-04-03 12:19:48 +1100

    Refactored the router to be slightly more logical and maintainable.

--- source/routers/v3/router.js
+++ source/routers/v3/router.js
@@ -4,113 +4,134 @@ define(
     "use strict";
     var Router = Backbone.Router.extend({
       initialize: function () {
+        app.router = this;
         $(document).on('pagebeforeload', function (e, data) {
           e.preventDefault();
           $.mobile.loading('show');
 
-          var path = data.dataUrl.substr(1).split('/'),
-            answerspace,
-            interaction,
-            args,
-            end,
-            finalparam,
-            parent = "app",
-            index,
-            tempmodel,
-            model,
-            promises = [],
-            assembleArgs = function (argstring) {
-              var argarray = argstring.split('&'),
-                args = {};
-              $.each(argarray, function (index, string) {
-                var equalIndex, name, value;
-                if (string.length !== 0 && (equalIndex = string.indexOf('=')) !== -1) {
-                  name = string.substring(0, equalIndex);
-                  value = string.substring(equalIndex + 1);
-                  if (value) {
-                    args[decodeURIComponent(name)] = decodeURIComponent(value);
-                  }
-                }
-              });
-              return args;
-            };
+          app.router.processPath(data);
+        });
+      },
 
-          if (path.length === 1) {
-            if (app.has("homeScreen") && app.get("homeScreen") === true) {
-              answerspace = app.get("siteName");
-              interaction = app.get("homeInteraction");
-              args = "";
-            } else {
-              answerspace = app.get("siteName");
-              interaction = app.get("siteName");
-              args = "";
-            }
-            path = [];
+      processPath: function (data) {
+        var path = data.dataUrl.substr(1).split('/'),
+          answerspace,
+          interaction,
+          args,
+          end,
+          finalparam,
+          parent = "app",
+          index,
+          promises = [];
+
+        if (path.length === 1) {
+          // Home page of app
+          if (app.has("homeScreen") && app.get("homeScreen") === true) {
+            answerspace = app.get("siteName");
+            interaction = app.get("homeInteraction");
+            args = "";
           } else {
-            answerspace = path.shift();
-            end = path.pop();
-            if (end && end.indexOf('?') !== -1 && end.indexOf('?') !== 0) {
-              finalparam = end.split('?');
-              args = assembleArgs(finalparam.pop());
-              interaction = finalparam.pop();
-            } else if (end && end.indexOf('?') !== -1 && end.indexOf('?') === 0) {
-              interaction = path.pop();
-              args = assembleArgs(end.slice(1));
-            } else {
-              args = null;
-              interaction = end;
+            answerspace = app.get("siteName");
+            interaction = app.get("siteName");
+            args = "";
+          }
+          path = [];
+        } else {
+          // Need to parse interaction + dependancies
+          answerspace = path.shift();
+          end = path.pop();
+          if (end && end.indexOf('?') !== -1 && end.indexOf('?') !== 0) {
+            finalparam = end.split('?');
+            args = this.assembleArgs(finalparam.pop());
+            interaction = finalparam.pop();
+          } else if (end && end.indexOf('?') !== -1 && end.indexOf('?') === 0) {
+            interaction = path.pop();
+            args = this.assembleArgs(end.slice(1));
+          } else {
+            args = null;
+            interaction = end;
+          }
+        }
+
+        // Load any dependancies
+        if (path.length > 0) {
+          for (index = 0; index < path.length; index = index + 1) {
+            promises.push(app.router.loadInteraction(path[index], null, parent));
+            parent = path[index];
+          }
+        }
+
+        // Load final interaction
+        $.when.apply($, promises).then(function () {
+          app.router.loadInteraction(interaction, args, parent, app.router.displayInteraction(data));
+        });
+      },
+
+      assembleArgs: function (argstring) {
+        var argarray = argstring.split('&'),
+          args = {};
+        $.each(argarray, function (index, string) {
+          var equalIndex, name, value;
+          if (string.length !== 0 && (equalIndex = string.indexOf('=')) !== -1) {
+            name = string.substring(0, equalIndex);
+            value = string.substring(equalIndex + 1);
+            if (value) {
+              args[decodeURIComponent(name)] = decodeURIComponent(value);
             }
           }
+        });
+        return args;
+      },
 
-          if (path.length > 0) {
-            for (index = 0; index < path.length; index = index + 1) {
-              tempmodel = new InteractionModel({
-                name: path[index],
-                parent: parent,
-                siteName: app.get("siteName"),
-                BICtype: "Interaction"
-              });
-              app.interactions.add(tempmodel);
-              promises.push(tempmodel.fetch());
-              parent = tempmodel.cid;
-            }
+      loadInteraction: function (interaction, args, parent, options) {
+        var dfrd, promise;
+        // Find if in collection
+        if (app.interactions.get(interaction)) {
+          if (app.interactions.get(interaction).get("type") === "madl code" && options) {
+            promise = app.interactions.get(interaction).fetch(options);
+          } else {
+            dfrd = $.Deferred();
+            promise = dfrd.promise();
+            dfrd.resolve();
           }
+          app.interactions.get(interaction).set("parent", parent);
+        } else {
+          app.interactions.add({
+            _id: interaction,
+            name: interaction,
+            parent: parent,
+            siteName: app.get("siteName"),
+            BICtype: "Interaction",
+            args: args
+          });
+          promise = app.interactions.get(interaction).fetch(options);
+        }
+          // Fetch if MADL
+        // Fetch and add to collection
+        return promise;
+      },
 
-          $.when.apply($, promises).then(function () {
-            model = new InteractionModel({
-              name: interaction,
-              parent: parent,
-              siteName: app.get("siteName"),
-              BICtype: "Interaction",
-              args: args
-            });
-            app.interactions.add(model);
-            model.fetch({
-              success: function (model, response, options) {
-                //model.inherit();
-                var view = new InteractionView({
-                  tagName: 'div',
-                  model: model
-                }).render();
-                view.$el.attr("data-url", options.data.dataUrl);
-                view.$el.attr("data-external-page", true);
-                view.$el.one('pagecreate', $.mobile._bindPageRemove);
-                options.data.deferred.resolve(options.data.absUrl, options.data.options, view.$el);
-              },
-              error: function (model, xhr, options) {
-                options.data.deferred.reject(options.data.absUrl, options.data.options);
-                $.mobile.showPageLoadingMsg($.mobile.pageLoadErrorMessageTheme, $.mobile.pageLoadErrorMessage, true);
-                setTimeout($.mobile.hidePageLoadingMsg, 1500);
-              },
-              data: data,
-              app: app
-            });
-          }, function () {
-            data.deferred.reject(data.absUrl, data.options);
+      displayInteraction: function (data) {
+        return {
+          success: function (model, response, options) {
+            //model.inherit();
+            var view = new InteractionView({
+              tagName: 'div',
+              model: model
+            }).render();
+            view.$el.attr("data-url", options.data.dataUrl);
+            view.$el.attr("data-external-page", true);
+            view.$el.one('pagecreate', $.mobile._bindPageRemove);
+            options.data.deferred.resolve(options.data.absUrl, options.data.options, view.$el);
+          },
+          error: function (model, xhr, options) {
+            options.data.deferred.reject(options.data.absUrl, options.data.options);
             $.mobile.showPageLoadingMsg($.mobile.pageLoadErrorMessageTheme, $.mobile.pageLoadErrorMessage, true);
             setTimeout($.mobile.hidePageLoadingMsg, 1500);
-          });
-        });
+          },
+          data: data,
+          app: app
+        };
       }
     });
 
