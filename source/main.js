cc42af478db8083da626bde8fd6294b8cd8c6177
Ben Barclay <ben.barclay@blinkmobile.com.au>
2013-04-17 16:40:49 +1000

    Removed all config options specified in the global Require.js

--- source/main.js
+++ source/main.js
@@ -2,30 +2,12 @@
 requirejs.config({
   baseUrl: '/_BICv3_/source',
   paths: {
-    text: ['https://d1c6dfkb81l78v.cloudfront.net/requirejs/2.1.2/text'],
-    jquery: ['https://d1c6dfkb81l78v.cloudfront.net/jquery/1.8.3/jq.min'],
-    jquerymobile: ['https://d1c6dfkb81l78v.cloudfront.net/jquery.mobile/1.3.0/jqm.min'],
-    underscore: ['https://d1c6dfkb81l78v.cloudfront.net/underscorejs/1.4.3/u.min'],
-    backbone: ['https://d1c6dfkb81l78v.cloudfront.net/backbonejs/0.9.10/backbone.min'],
-    mustache: ['https://d1c6dfkb81l78v.cloudfront.net/mustache/0.7.2/mustache.min'],
     BlinkForms: ['/_BICv3_/assets/js/BlinkForms.min'],
-    rivets: ['https://d1c6dfkb81l78v.cloudfront.net/rivets/0.4.5/rivets.min'],
-    q: ['https://d1c6dfkb81l78v.cloudfront.net/q/0.8.11/q.min'],
     pouchdb: ['/_BICv3_/assets/js/pouchdb-nightly']
   },
   shim: {
-    'underscore': {
-      exports: '_'
-    },
-    'backbone': {
-      deps: ['underscore', 'jquery'],
-      exports: 'Backbone'
-    },
     'BlinkForms': {
       exports: 'BlinkForms'
-    },
-    'rivets': {
-      exports: 'rivets'
     },
     'pouchdb': {
       exports: 'Pouch'
