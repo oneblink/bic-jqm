5eb55fb2d9a6257e8533e8d76743d7ebba3df8d4
Ben Barclay <ben.barclay@blinkmobile.com.au>
2013-05-20 11:46:49 +1000

    Officially deprecating all the old BICv3 php code.
    The system has a little php left in index.php, but otherwise completely runs off javascript now.

--- index.php
+++ index.php
@@ -1,28 +1,8 @@
 <!DOCTYPE html>
 <?php
-// Ron's BIC Thang && BB's BIC Components
 require_once('vendor/autoload.php');
-
-// Pull in the CDN's
-\Blink\cdn\PlatformCDN::setConfig(BlinkPlatformConfig::$CDN_PLATFORM);
-$cdnp = new \Blink\cdn\PlatformCDN();
-
-$defaultLoc = \Blink\cdn\CDN_Factory::getDefaultLocation($answer_space_id);
 $cdna = \Blink\cdn\CDN_Factory::openCDN($asConfig['cdnLocation'], $answer_space_id);
-
-$renderer = new \Blink\bic\Renderer();
-$handler = new \Blink\bic\RequestHandler();
-$router = new \Blink\bic\Router();
-
-$getConfigs = new \Blink\bic\GetConfigs();
-
-$content = $router->route($_SERVER['REQUEST_URI'], $_REQUEST, $handler, $renderer, $answer_space_id, $asConfig, $cdnp, $cdna, $getConfigs);
-
-if (array_key_exists('themeSwatch', $content) && $content['themeSwatch']){ 
-    $attrs = 'data-theme="' . $content['themeSwatch'] . '"';
-} else {
-    $attrs = '';
-}
+$config = json_decode($asFields['config']);
 ?>
 
 <html>
@@ -31,15 +11,15 @@ if (array_key_exists('themeSwatch', $content) && $content['themeSwatch']){
         <meta name="viewport" content="initial-scale=1">
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <?php
-        if (array_key_exists('themePack', $content) && strlen($content['themePack']) > 0) {
-            echo '<link rel="stylesheet" href="' . $cdna->getURI($asConfig['themePack']) . '" />';
+        if (isset($config->default->themePack)) {
+            echo '<link rel="stylesheet" href="' . $cdna->getURI($config->default->themePack) . '" />';
         } else {
-            if (array_key_exists('platformCSS', $content) && strlen($content['platformCSS']) > 0 && $content['platformCSS'] === 'default') {
+            if (isset($config->default->platformCSS) && $config->default->platformCSS === 'default') {
                 echo '<link rel="stylesheet" href="https://d1c6dfkb81l78v.cloudfront.net/jquery.mobile/1.3.0/jqm.theme.min.css" />';
             }
         }
 
-        if (array_key_exists('platformCSS', $content) && strlen($content['platformCSS']) > 0 && $content['platformCSS'] !== 'none') {
+        if (isset($config->default->platformCSS) && $config->default->platformCSS !== 'none') {
             echo '<link rel="stylesheet" href="https://d1c6dfkb81l78v.cloudfront.net/jquery.mobile/1.3.0/jqm.structure.min.css" />';
         }
         ?>
@@ -55,22 +35,22 @@ if (array_key_exists('themeSwatch', $content) && $content['themeSwatch']){
         </script>
         <script data-main="/_BICv3_/scripts/main" src="https://d1c6dfkb81l78v.cloudfront.net/blink/require/1/require.min.js"></script>
         <?php
-        if(array_key_exists('externalJavaScript', $content) && strlen($content['externalJavaScript']) > 0){
-           $js = explode("\n", $content['externalJavaScript']);
+        if(isset($config->default->externalJavaScript)){
+           $js = explode("\n", $config->default->externalJavaScript);
            foreach ($js as $jsitem) {
              echo '<script src="' . $jsitem . '"></script>';
            }
         }
         
-        if(array_key_exists('externalCSS', $content) && strlen($content['externalCSS']) > 0){
-           $css = explode("\n", $content['externalCSS']);
+        if(isset($config->default->externalCSS)){
+           $css = explode("\n", $config->default->externalCSS);
            foreach ($css as $cssitem) {
               echo '<link rel="stylesheet" href="' . $cssitem . '" />';
            }
         }
         
-        if(array_key_exists('styleSheet', $content) && strlen($content['styleSheet']) > 0){
-           echo '<style type="text/css">' . $content['styleSheet'] . '</style>';
+        if(isset($config->default->styleSheet)){
+           echo '<style type="text/css">' . $config->default->styleSheet . '</style>';
         }
         ?>
     </head>
