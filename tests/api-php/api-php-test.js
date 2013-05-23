2adea3fbe6881745caedbe645a42684618d9772c
Ben Barclay <ben.barclay@blinkmobile.com.au>
2013-05-23 10:07:35 +1000

    Fully passing test suite for API

--- tests/api-php/api-php-test.js
+++ tests/api-php/api-php-test.js
@@ -10,18 +10,30 @@ define(['../../scripts/api-php.js'],
   function (api) {
     "use strict";
     describe('API Facade Layer - PHP Implementation', function () {
-      var server;
+      var server, handler;
 
       before(function () {
         server = sinon.fakeServer.create();
         server.respondWith('/_R_/common/3/xhr/GetConfig.php', [200, { "Content-Type": "application/json", "Content-Length": 10 }, '{"_id": 1}']);
-        server.respondWith('/_R_/common/3/xhr/GetAnswer.php?asn=' + window.BMP.siteVars.answerSpace + '&iact=Exists&ajax=false', [200, { "Content-Type": "application/json", "Content-Length": 10 }, '{"_id": 1}']);
+        server.respondWith('get', '/_R_/common/3/xhr/GetAnswer.php?asn=' + window.BMP.siteVars.answerSpace + '&iact=Exists&ajax=false', [200, { "Content-Type": "application/json", "Content-Length": 10 }, '{"_id": 1}']);
+        server.respondWith('get', '/_R_/common/3/xhr/GetAnswer.php?asn=' + window.BMP.siteVars.answerSpace + '&iact=Exists&ajax=false&args[0]=Exists', [200, { "Content-Type": "application/json", "Content-Length": 10 }, '{"_id": 3}']);
+        server.respondWith('post', '/_R_/common/3/xhr/GetAnswer.php?asn=' + window.BMP.siteVars.answerSpace + '&iact=Exists&ajax=false', [200, { "Content-Type": "application/json", "Content-Length": 10 }, '{"_id": 2}']);
         server.respondWith('/_R_/common/3/xhr/GetMoJO.php?_id=' + window.BMP.siteVars.answerSpaceId + '&_m=Exists&_lc=1', [200, { "Content-Type": "application/json", "Content-Length": 10 }, '{"_id": 1}']);
         server.respondWith('/_BICv3_/xhr/GetForm.php?asn=' + window.BMP.siteVars.answerSpace + '&form=Exists', [200, { "Content-Type": "application/json", "Content-Length": 10 }, '{"_id": 1}']);
+        server.respondWith('post', '/_R_/common/3/xhr/SaveFormRecord.php?_asid=' + window.BMP.siteVars.answerSpaceId + '&_fn=Exists&_action=Exists', [200, { "Content-Type": "application/json", "Content-Length": 10 }, '{"_id": 1}']);
 
         $.ajaxPrefilter(function (options, originalOptions, jqXHR) {
           jqXHR.setRequestHeader('X-Blink-Config', JSON.stringify(window.BMP.siteVars));
         });
+
+        handler = function (data, status, xhr) {
+          should.exist(data);
+          should.exist(xhr);
+          should.exist(status);
+          data.should.not.be.empty;
+          status.should.be.string('success');
+          xhr.should.be.an('object');
+        };
       });
 
       after(function () {
@@ -35,81 +47,87 @@ define(['../../scripts/api-php.js'],
       describe('getAnswerSpaceMap()', function () {
         it('should GET a JSON definition from the server', function (done) {
           api.getAnswerSpaceMap()
-            .done(function (data, status, xhr) {
-              expect(data).to.be.an('object');
-              expect(xhr).to.be.an('object');
-              expect(status).to.be.string('success');
+            .done(handler)
+            .done(function () {
               done();
             });
           server.respond();
         });
-        it('should crash and burn if the params are wrong');
       });
 
-      describe('getInteractionResult(iact, options)', function () {
+      describe('getInteractionResult(iact, args, options)', function () {
         it('should GET a JSON definition from the server', function (done) {
-          api.getInteractionResult('Exists', 'Exists')
-            .done(function (data, status, xhr) {
-              expect(data).to.be.an('object');
-              expect(xhr).to.be.an('object');
-              expect(status).to.be.string('success');
+          api.getInteractionResult('Exists', null)
+            .done(handler)
+            .done(function () {
               done();
             });
           server.respond();
         });
-        it('should handle GET data passed in via args');
-        it('should handle POST data passed in via options param');
+
+        it('should handle GET data passed in via args', function () {
+          api.getInteractionResult('Exists', {
+            0: "Exists"
+          })
+            .done(handler)
+            .done(function (data, status, xhr) {
+              data._id.should.equal(3);
+            })
+            .done(function () {
+              done();
+            });
+          server.respond();
+        });
+
+        it('should handle POST data passed in via options param', function (done) {
+          api.getInteractionResult('Exists', null, {
+            type: "POST",
+            data: {
+              Exists: "Exists"
+            }
+          })
+            .done(handler)
+            .done(function (data, status, xhr) {
+              data._id.should.equal(2);
+            })
+            .done(function () {
+              done();
+            });
+          server.respond();
+        });
       });
 
-      describe('getDataSuitcase', function () {
+      describe('getDataSuitcase(suitcase, time)', function () {
         it('should GET a JSON definition from the server', function (done) {
           api.getDataSuitcase('Exists', '1')
-            .done(function (data, status, xhr) {
-              expect(data).to.be.an('string');
-              expect(xhr).to.be.an('object');
-              expect(status).to.be.string('success');
-              done();
-            });
-          server.respond();
-        });
-        it('should crash and burn if the params are wrong', function (done) {
-          api.getDataSuitcase('NotExists')
-            .fail(function (xhr, status, error) {
-              expect(xhr).to.be.an('object');
-              expect(status).to.be.string('error');
-              expect(error).to.be.string('Not Found');
+            .done(handler)
+            .done(function () {
               done();
             });
           server.respond();
         });
       });
 
-      describe('getForm', function () {
+      describe('getForm(form)', function () {
         it('should GET a JSON definition from the server', function (done) {
           api.getForm('Exists', 'Exists')
-            .done(function (data, status, xhr) {
-              expect(data).to.be.an('object');
-              expect(xhr).to.be.an('object');
-              expect(status).to.be.string('success');
-              done();
-            });
-          server.respond();
-        });
-        it('should crash and burn if the params are wrong', function (done) {
-          api.getForm('NotExists')
-            .fail(function (xhr, status, error) {
-              expect(xhr).to.be.an('object');
-              expect(status).to.be.string('error');
-              expect(error).to.be.string('Not Found');
+            .done(handler)
+            .done(function () {
               done();
             });
           server.respond();
         });
       });
 
-      describe('setPendingItem', function () {
-        it('should POST a JSON definition to the server');
-        it('should crash and burn if the params are wrong');
+      describe('setPendingItem(form, action, data)', function () {
+        it('should POST data to the server', function (done) {
+          api.setPendingItem('Exists', 'Exists', {Exists: "Exists"})
+            .done(handler)
+            .done(function () {
+              done();
+            });
+          server.respond();
+        });
       });
     });
   });
