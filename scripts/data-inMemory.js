d7b20547e879452ecc1544facfc91079d03443e3
Ben Barclay <benbarclay@gmail.com>
2013-06-11 15:19:22 +1000

    inMemory data layer rejects all promises

--- scripts/data-inMemory.js
+++ scripts/data-inMemory.js
@@ -5,7 +5,34 @@ define(
 
     var Data = function () {};
 
-    _.extend(Data.prototype, {});
+    _.extend(Data.prototype, {
+      data: {},
+
+      create: function (model) {
+        return new $.Deferred().reject().promise();
+      },
+
+      update: function (model) {
+        return new $.Deferred().reject().promise();
+      },
+
+      read: function (model) {
+        return new $.Deferred().reject().promise();
+      },
+
+      readAll: function () {
+        return new $.Deferred().reject().promise();
+      },
+
+      delete: function (model) {
+        return new $.Deferred().reject().promise();
+      },
+
+      deleteAll: function (model) {
+        return new $.Deferred().reject().promise();
+      }
+
+    });
 
     return Data;
   }
