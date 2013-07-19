da2563445a8ed14ef9d70f9d616c4f76984aa83f
Ben Barclay <benbarclay@gmail.com>
2013-07-19 12:41:08 +1000

    Updates to the unit tests

--- tests/test-router.js
+++ tests/test-router.js
@@ -2,22 +2,38 @@
 define(function () {
   "use strict";
   describe('Router - jQuery Mobile Implementation', function () {
-    var router, originalModel, originalView;
+    var router, originalModel, originalView, viewSpy, interactionsGetStub, interactionSetSpy;
 
     before(function (done) {
       require(['model-application', 'view-interaction'], function (app, InteractionView) {
+        var interactionModel;
 
         originalModel = app;
         originalView = InteractionView;
         requirejs.undef('model-application');
         requirejs.undef('view-interaction');
 
+        app = new Backbone.Model();
+        app.interactions = new Backbone.Collection();
+        interactionsGetStub = sinon.stub(app.interactions, "get");
+
+        interactionModel = new Backbone.Model();
+        interactionModel.set({'id': function () {return "twelve"}()})
+        interactionSetSpy = sinon.spy(interactionModel, "set");
+        app.interactions.get.returns(interactionModel);
+
         define('model-application', [], function () {
-          return function (param) {console.log(param)};
+          return app;
         });
 
+        var view = Backbone.View.extend({
+          render: function () {
+            this.trigger('render');
+          }
+        });
+        viewSpy = sinon.spy(view);
         define('view-interaction', [], function () {
-          return function (param) {console.log(param)};
+          return viewSpy;
         });
 
         require(['router'], function (rRouter) {
@@ -39,47 +55,96 @@ define(function () {
         });
     });
 
-    it("should exist", function () {
-      should.exist(router);
-    });
-
-    describe('initialize', function () {
-      it("should contain the function initialize", function () {
-        router.should.respondTo('initialize');
-      });
-
-      it("should bind to pagebeforeload", function () {
+    describe('initialize()', function () {
+      it("should create binding to pagebeforeload that passes the event data object to routeRequest", function () {
         sinon.stub(router, "routeRequest");
-        $(document).trigger('pagebeforeload');
+        $(document).trigger('pagebeforeload', {});
         router.routeRequest.called.should.be.true;
+        router.routeRequest.calledWith({}).should.be.true;
         router.routeRequest.restore();
       });
-
-      it("should prevent the default action");
-
-      it("should detect if the interaction is specified by ID (instead of name)");
-
-      it("should prepare the inheritance chain");
-
-      it("should prepare any GET attributes");
-
-      it("should tell the model to prepare for the view");
-
-      it("should create the view");
-
-      it("should bind view attribute creation to render event");
-
-      it("should resolve the deferred on render event");
-
-      it("should start the render process");
     });
 
-    describe('inheritanceChain(data)', function () {
-      it("should contain the function inheritanceChain", function () {
-        router.should.respondTo('inheritanceChain');
+    describe('routeRequest(data)', function () {
+      var model, dfrd;
+
+      before(function () {
+        var stub;
+        stub = sinon.stub(router, "inheritanceChain");
+        sinon.stub(router, "parseArgs");
+        model = new Backbone.Model;
+        model.prepareForView = function () {
+          return $.Deferred().resolve(this).promise();
+        };
+        sinon.spy(model, "prepareForView");
+        stub.returns(model);
+
+        //This is slightly cheating, but saves me doing it over and over again
+        dfrd = new $.Deferred();
+        router.routeRequest({
+          dataUrl: "/test",
+          deferred: dfrd
+        });
+
       });
 
-      it("should set the inheritance chain on relevant interactions");
+      after(function () {
+        router.inheritanceChain.restore();
+        router.parseArgs.restore();
+      });
+
+      it("should call the inheritanceChain function to get the correct interaction model", function () {
+        router.inheritanceChain.called.should.be.true;
+        router.inheritanceChain.reset();
+      });
+
+      it("should start the parseArgs function", function () {
+        router.parseArgs.called.should.be.true;
+        router.parseArgs.reset();
+      });
+
+      it("should instruct the model to prepareForView", function () {
+        model.prepareForView.called.should.be.true;
+        model.prepareForView.reset();
+      });
+
+      it("should create a new view", function () {
+        viewSpy.called.should.be.true;
+        viewSpy.reset();
+      });
+
+      it("should resolve the event deferred (transitioning the view onscreen)", function (done) {
+        dfrd.always(function () {
+          done();
+        });
+      });
+
+      it("should remove old views from the DOM");
+    });
+
+    describe('inheritanceChain(urlString)', function () {
+      it("should parse the path given for interactions");
+      it("should detect interactions specified by ID (instead of name)");
+      it("should use the path to determine the inheritance chain");
+
+      it("should set the parent of each interaction in the chain on the associated model", function () {
+        var interaction = router.inheritanceChain("/test/second");
+        interaction.get('parent').should.be.string(interactionSetSpy.secondCall.args[0].parent);
+      });
+
+      it("should set the topmost interaction in the chain to have a parent of the answerspace", function () {
+        router.inheritanceChain("/test").get('parent').should.be.string("app");
+      });
+
+      it("should return the correct interaction to attach the view to", function () {
+        router.inheritanceChain("/test").should.be.instanceOf(Backbone.Model);
+      });
+    });
+
+    describe('parseArgs(argsString, model)', function () {
+      it("should identify any GET arguments in the input string");
+      it("should parse found arguments into an object");
+      it("should set the object on the model as 'args'");
     });
   });
 });
