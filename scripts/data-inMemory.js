251c968104f8ec21b79edbeaa980981f8475222e
Ben Barclay <benbarclay@gmail.com>
2014-06-12 12:08:05 +1000

    Add a reason for why data-inMemory promises are rejected

--- scripts/data-inMemory.js
+++ scripts/data-inMemory.js
@@ -10,23 +10,23 @@ define(
 
     _.extend(Data.prototype, {
       create: function () {
-        return Promise.reject();
+        return Promise.reject('Persistent storage not available');
       },
 
       update: function () {
-        return Promise.reject();
+        return Promise.reject('Persistent storage not available');
       },
 
       read: function () {
-        return Promise.reject();
+        return Promise.reject('Persistent storage not available');
       },
 
       readAll: function () {
-        return Promise.reject();
+        return Promise.reject('Persistent storage not available');
       },
 
       'delete': function () {
-        return Promise.reject();
+        return Promise.reject('Persistent storage not available');
       }
     });
 
