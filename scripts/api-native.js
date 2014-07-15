02b3f1adf2e64f3a12ad641ab49e0f7984768aba
Ben Barclay <benbarclay@gmail.com>
2014-07-15 10:47:40 +1000

    Some initial work on the android offline plugin API

--- scripts/api-native.js
+++ scripts/api-native.js
@@ -1,19 +1,98 @@
+/*global cordova: true */
 define(
   [],
   function () {
     "use strict";
     var API = {
-      getAnswerSpaceMap: function () {},
-      getInteractionResult: function (iact, args, options) {},
-      getForm: function () {},
-      getDataSuitcase: function (suitcase, time) {},
-      setPendingItem: function (formname, formaction, formdata) {},
-      getLoginStatus: function () {},
-      getFormList: function (formName) {},
-      getFormRecord: function (formName, formAction, recordID) {}
+      getAnswerSpaceMap: function (options) {
+        cordova.offliner.retrieveContent(
+          options.success,
+          options.error,
+          {
+            url: '/_R_/common/3/xhr/GetConfig.php'
+          }
+        );
+      },
+
+      getInteractionResult: function (options) {
+        //return $.ajax('/_R_/common/3/xhr/GetAnswer.php?asn=' + window.BMP.BIC.siteVars.answerSpace + '&iact=' + iact + '&ajax=false' + getargs, options);
+        cordova.offliner.retrieveContent(
+          options.success,
+          options.error,
+          {
+            url: '/_R_/common/3/xhr/GetConfig.php'
+          }
+        );
+      },
+
+      getForm: function (options) {
+        //return $.ajax('/_R_/common/3/xhr/GetForm.php?_v=3');
+        cordova.offliner.retrieveContent(
+          options.success,
+          options.error,
+          {
+            url: '/_R_/common/3/xhr/GetConfig.php'
+          }
+        );
+      },
+
+      getDataSuitcase: function (options) {
+        //return $.ajax('/_R_/common/3/xhr/GetMoJO.php?_id=' + window.BMP.BIC.siteVars.answerSpaceId + '&_m=' + suitcase + '&_lc=' + time, {dataType: "text"});
+        cordova.offliner.retrieveContent(
+          options.success,
+          options.error,
+          {
+            url: '/_R_/common/3/xhr/GetConfig.php'
+          }
+        );
+      },
+
+      setPendingItem: function (options) {
+        //return $.post('/_R_/common/3/xhr/SaveFormRecord.php?_asid=' + window.BMP.BIC.siteVars.answerSpaceId + '&_fn=' + formname + '&_action=' + formaction, formdata);
+        cordova.offliner.retrieveContent(
+          options.success,
+          options.error,
+          {
+            url: '/_R_/common/3/xhr/GetConfig.php'
+          }
+        );
+      },
+
+      getLoginStatus: function (options) {
+        //return $.ajax('/_R_/common/3/xhr/GetLogin.php');
+        cordova.offliner.retrieveContent(
+          options.success,
+          options.error,
+          {
+            url: '/_R_/common/3/xhr/GetConfig.php'
+          }
+        );
+      },
+
+      getFormList: function (options) {
+        //return $.ajax('/_R_/common/3/xhr/GetFormList.php?_asid=' + window.BMP.BIC.siteVars.answerSpaceId + '&_fn=' + formName);
+        cordova.offliner.retrieveContent(
+          options.success,
+          options.error,
+          {
+            url: '/_R_/common/3/xhr/GetConfig.php'
+          }
+        );
+      },
+
+      getFormRecord: function (options) {
+        //return $.ajax('/_R_/common/3/xhr/GetFormRecord.php?_fn=' + formName + '&_tid=' + recordID + '&action=' + formAction);
+        cordova.offliner.retrieveContent(
+          options.success,
+          options.error,
+          {
+            url: '/_R_/common/3/xhr/GetConfig.php'
+          }
+        );
+      }
+
     };
 
     return API;
   }
 );
-
