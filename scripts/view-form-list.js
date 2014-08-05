2c337fb62546723259b16d0d49ead9cabc97b96a
Geeta <geeta@blinkmobile.com.au>
2014-08-05 16:51:42 +1000

    Forms-61 :  fixed form list

--- scripts/view-form-list.js
+++ scripts/view-form-list.js
@@ -9,47 +9,51 @@ define(
         app.formRecords.pull(view.model.get("blinkFormObjectName")).then(
           function () {
             var templateData = {};
-
-            templateData.content = _.map(app.formRecords.models, function (value) {
-              var record = {};
-
-              record.id = value.get("id");
-              record.contents = [];
-
-              _.each(value.attributes.list, function (iv, ik) {
-                if (ik !== 'id' && ik !== '_id') {
-                  record.contents.push(iv);
+            templateData.headers = [];
+            BlinkForms.getDefinition(view.model.get("blinkFormObjectName"), view.model.get("blinkFormAction")).then(function (definition) {
+              var elements = [];
+              /*jslint unparam: true */
+              _.each(definition._elements, function (value, ik) {
+                if (value.type !== 'subForm') {
+                  elements.push(value.name);
+                  templateData.headers.push(value.name);
                 }
               });
+              /*jslint unparam: false */
 
-              return record;
+              templateData.content = _.map(app.formRecords.models, function (value) {
+                var record = {};
+
+                record.id = value.get("id");
+                record.contents = [];
+
+                _.each(value.attributes.list, function (iv, ik) {
+                  if (ik !== 'id' && ik !== '_id' && _.contains(elements, ik)) {
+                    record.contents.push(iv);
+                  }
+                });
+
+                return record;
+              });
+
+              templateData.interactions = {};
+              templateData.interactions.edit = app.interactions.findWhere({
+                blinkFormObjectName: view.model.get("blinkFormObjectName"),
+                blinkFormAction: "edit"
+              }).id;
+              templateData.interactions.view = app.interactions.findWhere({
+                blinkFormObjectName: view.model.get("blinkFormObjectName"),
+                blinkFormAction: "view"
+              }).id;
+              templateData.interactions['delete'] = app.interactions.findWhere({
+                blinkFormObjectName: view.model.get("blinkFormObjectName"),
+                blinkFormAction: "delete"
+              }).id;
+
+              view.$el.html(Mustache.render(Template, templateData));
+              view.trigger('render');
+
             });
-
-            templateData.headers = [];
-            /*jslint unparam: true */
-            _.each(app.formRecords.at(0).attributes.list, function (value, key) {
-              if (key !== 'id' && key !== '_id') {
-                templateData.headers.push(key);
-              }
-            });
-            /*jslint unparam: false */
-
-            templateData.interactions = {};
-            templateData.interactions.edit = app.interactions.findWhere({
-              blinkFormObjectName: view.model.get("blinkFormObjectName"),
-              blinkFormAction: "edit"
-            }).id;
-            templateData.interactions.view = app.interactions.findWhere({
-              blinkFormObjectName: view.model.get("blinkFormObjectName"),
-              blinkFormAction: "view"
-            }).id;
-            templateData.interactions['delete'] = app.interactions.findWhere({
-              blinkFormObjectName: view.model.get("blinkFormObjectName"),
-              blinkFormAction: "delete"
-            }).id;
-
-            view.$el.html(Mustache.render(Template, templateData));
-            view.trigger('render');
           },
           function () {
             view.$el.html('Cannot contact server');
@@ -64,7 +68,3 @@ define(
     return FormListView;
   }
 );
-
-
-
-
