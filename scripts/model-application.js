1252686d89238608ba6893651f12815bb64f9958
Ben Barclay <benbarclay@gmail.com>
2014-08-27 16:42:41 +1000

    Version update

--- scripts/model-application.js
+++ scripts/model-application.js
@@ -154,7 +154,7 @@ define(
       window.BMP.BIC3.history.length += 1;
     };
 
-    window.BMP.BIC3.version = '3.1.11';
+    window.BMP.BIC3.version = '3.1.12';
 
     return window.BMP.BIC3;
   }
