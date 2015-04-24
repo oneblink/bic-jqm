d678ad4963630c19dd4a874ab9642d07251ba293
Ron Waldon <jokeyrhyme@gmail.com>
2015-04-24 14:27:15 +1000

    fix test server AppCache to be more like real Mobility Platform

--- tests/support/server.js
+++ tests/support/server.js
@@ -49,6 +49,8 @@ server.route({
     });
     contents = contents.replace('/_c_/blink/bic/{{id}}/bic.min.js\n', '');
     contents = contents.replace('/_c_/blink/bic/{{id}}/bic.js', '/bic.js');
+    contents = contents.replace(/^\/_c_\//mg, '//d1c6dfkb81l78v.cloudfront.net/');
+    contents += '\nCACHE:\n/_R_/common/3/xhr/GetConfig.php?_asn=integration\n';
     reply(contents).header('Content-Type', 'text/cache-manifest');
   }
 });
