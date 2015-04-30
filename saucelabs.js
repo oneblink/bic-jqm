6c9f8f8dc0af69a1614f78da53706fd8abcaa288
Ron Waldon <jokeyrhyme@gmail.com>
2015-04-30 13:52:53 +1000

    travis: adjust saucelabs configuration

--- saucelabs.js
+++ saucelabs.js
@@ -1,53 +1,46 @@
+/*eslint-env node*/
+/*eslint-disable no-process-env*/
 'use strict';
 
 // exports
 
 module.exports = {
   urls: [
-    'http://localhost:9999/tests/index.html'
+    'http://localhost:9998/tests/index.html'
   ],
   tunnelTimeout: 5,
-  build: process.env.TRAVIS_JOB_ID,
+  build: process.env.TRAVIS_BUILD_NUMBER,
   concurrency: 3,
   browsers: [
     {
       browserName: "internet explorer",
-      version: "11",
-      platform: "Windows 8.1"
-    },
-    {
-      browserName: "internet explorer",
-      version: "10",
-      platform: "Windows 8"
+      version: "11"
     },
     {
       browserName: "firefox",
-      version: "28",
-      platform: "Windows 7"
+      version: "37"
     },
     {
       browserName: "googlechrome",
-      version: "33",
-      platform: "OS X 10.9"
+      version: "41"
     },
     {
       browserName: "android",
-      version: "4.1",
-      platform: "Linux",
+      version: "4.3",
       "device-orientation": "portrait"
     },
     {
       browserName: "iphone",
-      version: "7.1",
-      platform: "OS X 10.9",
+      version: "8.2",
       "device-orientation": "portrait"
     },
     {
       browserName: "safari",
-      version: "6",
-      platform: "OS X 10.8"
+      version: "7"
     }
   ],
   testname: 'bic-v3',
-  tags: ['master']
+  tags: [
+    process.env.TRAVIS_BRANCH
+  ]
 };
