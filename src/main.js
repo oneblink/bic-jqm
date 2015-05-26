63d3a03118b7ed0de52cf7d2ae5f8662fc25fb6a
Ron Waldon <jokeyrhyme@gmail.com>
2015-05-26 13:51:26 +1000

    extract Backbone.sync() adjustments to separate module

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
 define(
-  ['model-application', 'authentication'],
-  function (app, Auth) {
+  ['model-application', 'authentication', 'backbone-fix'],
+  function (app, Auth, fixBackbone) {
     'use strict';
 
-    function start() {
+    function start () {
       // AJAX Default Options
       $.ajaxPrefilter(function (options, originalOptions, jqXHR) {
         jqXHR.setRequestHeader('X-Blink-Config',
@@ -15,65 +15,7 @@ define(
       require(['router', 'auth']);
     }
 
-    // Save traditional sync method as ajaxSync
-    Backbone.ajaxSync = Backbone.sync;
-
-    // New sync method
-    Backbone.dataSync = function (method, model, options) {
-      var data, promise;
-      data = model.data || model.collection.data;
-
-      switch (method) {
-      case 'read':
-        promise = model.id !== undefined ? data.read(model) : data.readAll();
-        break;
-      case 'create':
-        promise = data.create(model);
-        break;
-      case 'update':
-        promise = data.update(model);
-        break;
-      case 'patch':
-        promise = data.update(model);
-        break;
-      case 'delete':
-        promise = data.delete(model);
-        break;
-      default:
-        promise = Promise.reject(new Error('unknown method'));
-      }
-
-      promise.then(function (response) {
-        if (options.success) {
-          options.success(response);
-        }
-      }, function (response) {
-        if (options.error) {
-          options.error(response);
-        }
-      });
-
-      model.trigger('request', model, promise, options);
-
-      return promise;
-    };
-
-    // Fallback to traditional sync when not specified
-    Backbone.getSyncMethod = function (model) {
-      if (model.data || model.collection && model.collection.data) {
-        return Backbone.dataSync;
-      }
-      return Backbone.ajaxSync;
-    };
-
-    if (app.hasStorage()) {
-      // Hook Backbone.sync up to the data layer
-      Backbone.sync = function (method, model, options) {
-        return Backbone.getSyncMethod(model).apply(this, [method, model, options]);
-      };
-    } else {
-      Backbone.sync = function (method, model, options) { options.success(); };
-    }
+    fixBackbone(app);
 
     if (window.BMP.BlinkGap.isHere()) {
       // Delay the app for Cordova
