3923fe7111677f4a75ef937d3c7ea937bc05322d
Ron Waldon <jokeyrhyme@gmail.com>
2015-07-28 14:34:11 +1000

    bump version to v3.7.1

--- src/bic.js
+++ src/bic.js
@@ -46,7 +46,7 @@ define(function (require) {
   // keep BMP.BIC and BMP.BIC3 the same (for now, "BIC3" is deprecated)
   window.BMP.BIC3 = app;
 
-  window.BMP.BIC.version = '3.7.0';
+  window.BMP.BIC.version = '3.7.1';
 
   return app; // export BMP.BIC
 });
